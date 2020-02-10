67b1aa575d49226039a0e8c566de77f9bd926f9d
flip.flop <flip.flop@kakaocorp.com>
2020-02-10 17:40:48 +0900

    Add some initial files
    - update .podspec
    - add some initial files

---
+++ Example/Pods/Target Support Files/Pods-YYSimpleAlert_Example/Pods-YYSimpleAlert_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YYSimpleAlert_Example : NSObject
+@end
+@implementation PodsDummy_Pods_YYSimpleAlert_Example
+@end
---
+++ Example/Pods/Target Support Files/YYSimpleAlert/YYSimpleAlert-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_YYSimpleAlert : NSObject
+@end
+@implementation PodsDummy_YYSimpleAlert
+@end
