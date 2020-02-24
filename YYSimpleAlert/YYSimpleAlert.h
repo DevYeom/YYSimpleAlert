c801912a350a555e3eb74f8e3546da441196ddd3
flip.flop <flip.flop@kakaocorp.com>
2020-02-24 22:10:18 +0900

    Support carthage
    - add files for supporting carthage

---
+++ YYSimpleAlert/YYSimpleAlert.h
@@ -0,0 +1,19 @@
+//
+//  YYSimpleAlert.h
+//  YYSimpleAlert
+//
+//  Created by flip.flop on 2020/02/24.
+//  Copyright © 2020 flip. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YYSimpleAlert.
+FOUNDATION_EXPORT double YYSimpleAlertVersionNumber;
+
+//! Project version string for YYSimpleAlert.
+FOUNDATION_EXPORT const unsigned char YYSimpleAlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YYSimpleAlert/PublicHeader.h>
+
+
